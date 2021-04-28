182740f4a4cbaa22327ff9465b9e27671c6ab6e4
Allison Logan <allisonlogan@Logan-Family-iMac.local>
2021-04-28 12:30:00 -0400

    Updates shortcode name Adds to more shortcodes

--- inc/childshortcodes.php
+++ inc/childshortcodes.php
@@ -2,21 +2,23 @@
 /**
  * Child Theme shortcodes
  *
- *	[ufl-contact-card][/ufl-contact-card]
+ *	[ufl-image-card-left][/ufl-image-card-left]
+ *	[ufl-image-card-right][/ufl-image-card-right]
+ *	[ufl-text-card][/ufl-text-card]
  *	 
  * @package HWCOE_UFL_CHILD
  */
  
 
 /**
- * Add Contact Card
+ * Add Image Card Left
  * 
- * Example [ufl-contact-card][/ufl-contact-card]
+ * Example [ufl-image-card-left][/ufl-image-card-left]
  * @param  array $atts Shortcode attributes
  * @param  string [$content = ''] Content between shortcode tags
  * @return string Shortcode output
  */
-function hwcoe_ufl_contact_card($atts, $content = NULL ) {
+function hwcoe_ufl_image_card_left($atts, $content = NULL ) {
 	
 	extract( shortcode_atts( 
 		array(
@@ -32,33 +34,96 @@ function hwcoe_ufl_contact_card($atts, $content = NULL ) {
 	// Shortcode callbacks must return content, so use output buffering
 	ob_start();
 	?>
-		<?php if (!empty( $height )){ ?>
-			<div class="contact-card" style="min-height:<?php echo esc_html( $height ); ?>;">
-				<div class="col-md-5 contact-card-img">
-					<img src="<?php echo esc_url( $image[0] ); ?>" alt="" width="262px">
-				</div>
-				<div class="col-md-7 contact-card-text">
-				  	<?php if (!empty( $title )){ ?>
-						<h2><?php echo esc_html( $title ); ?></h2>
-					<?php } ?>
-					<?php echo wpautop( wp_kses_post( $content ) ); ?>
-				</div>
+		<div class="image-card-left" style="min-height:<?php echo esc_html( $height ); ?>;">
+			<div class="col-md-5 image-card-left-img">
+				<img src="<?php echo esc_url( $image[0] ); ?>" alt="" width="262px">
 			</div>
-		<?php } else { ?>
-			<div class="contact-card">
-				<div class="col-md-5 contact-card-img">
-					<img src="<?php echo esc_url( $image[0] ); ?>" alt="" width="262px">
-				</div>
-				<div class="col-md-7 contact-card-text">
-				  	<?php if (!empty( $title )){ ?>
-						<h2><?php echo esc_html( $title ); ?></h2>
-					<?php } ?>
-					<?php echo wpautop( wp_kses_post( $content ) ); ?>
-				</div>
+			<div class="col-md-7 image-card-left-text">
+				<?php if (!empty( $title )){ ?>
+					<h2><?php echo esc_html( $title ); ?></h2>
+				<?php } ?>
+				<?php echo wpautop( wp_kses_post( $content ) ); ?>
 			</div>
-		<?php } ?>
+		</div>
 
 	 <?php 
 	return ob_get_clean();
 }
-add_shortcode('ufl-contact-card', 'hwcoe_ufl_contact_card');
+add_shortcode('ufl-image-card-left', 'hwcoe_ufl_image_card_left');
+
+
+/**
+ * Add Image Card Right
+ * 
+ * Example [ufl-image-card-right][/ufl-image-card-right]
+ * @param  array $atts Shortcode attributes
+ * @param  string [$content = ''] Content between shortcode tags
+ * @return string Shortcode output
+ */
+function hwcoe_ufl_image_card_right($atts, $content = NULL ) {
+	
+	extract( shortcode_atts( 
+		array(
+			'title' => '',
+			'image' => '',
+			'height' => '',
+		), $atts )
+	);
+	 
+	// Support either image ID or image url
+	$image = ( is_numeric( $image ) )? wp_get_attachment_image( $image ) : array($image);
+	
+	// Shortcode callbacks must return content, so use output buffering
+	ob_start();
+	?>
+		<div class="image-card-right" style="min-height:<?php echo esc_html( $height ); ?>;">
+			<div class="col-md-7 image-card-right-text">
+				<?php if (!empty( $title )){ ?>
+					<h2><?php echo esc_html( $title ); ?></h2>
+				<?php } ?>
+				<?php echo wpautop( wp_kses_post( $content ) ); ?>
+			</div>
+			<div class="col-md-5 image-card-right-img">
+				<img src="<?php echo esc_url( $image[0] ); ?>" alt="" width="262px">
+			</div>
+		</div>
+
+	 <?php 
+	return ob_get_clean();
+}
+add_shortcode('ufl-image-card-right', 'hwcoe_ufl_image_card_right');
+
+/**
+ * Add Text Card
+ * 
+ * Example [ufl-text-card][/ufl-text-card]
+ * @param  array $atts Shortcode attributes
+ * @param  string [$content = ''] Content between shortcode tags
+ * @return string Shortcode output
+ */
+function hwcoe_ufl_text_card($atts, $content = NULL ) {
+	
+	extract( shortcode_atts( 
+		array(
+			'title' => '',
+			'height' => '',
+			'background' => '',
+		), $atts )
+	);
+	 
+	// Shortcode callbacks must return content, so use output buffering
+	ob_start();
+	?>
+		<div class="text-card" style="height:<?php echo esc_html( $height ); ?>; background:<?php echo esc_html( $background ); ?>;">
+			<div class="col-md-12 text-card-text">
+				<?php if (!empty( $title )){ ?>
+					<h2><?php echo esc_html( $title ); ?></h2>
+				<?php } ?>
+				<?php echo wpautop( wp_kses_post( $content ) ); ?>
+			</div>
+		</div>
+
+	 <?php 
+	return ob_get_clean();
+}
+add_shortcode('ufl-text-card', 'hwcoe_ufl_text_card');
